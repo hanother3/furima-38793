30da14afe6f181a4a390d3a0477da26e7acba05c
hanother3 <hanazawa.s58.1219@gmail.com>
2023-01-09 22:28:13 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :delivery_charge
+  belongs_to :area
+  belongs_to :delivery_time
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
