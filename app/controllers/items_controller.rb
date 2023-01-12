7e5afb3b538d9359be1766b1e12533368562db0d
hanother3 <hanazawa.s58.1219@gmail.com>
2023-01-12 19:08:49 +0900

    before_actrionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     return if current_user == @item.user
 
     redirect_to root_path
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -44,5 +42,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :image, :item_name, :item_description, :category_id, :item_condition_id, :delivery_charge_id, :area_id, :delivery_time_id, :price
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
