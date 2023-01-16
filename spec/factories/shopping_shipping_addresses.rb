0e3398d834933c8b1e0b9f88f6eea6952c9ca4bc
hanother3 <hanazawa.s58.1219@gmail.com>
2023-01-16 18:30:34 +0900

    Formオブジェクトのテストコードを実装

---
+++ spec/factories/shopping_shipping_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :shopping_shipping_address do
+    postal_code          { '123-4567' }
+    area_id              { 2 }
+    city                 { '練馬区' }
+    house_number         { '1-1' }
+    building_name        { '東京ハイツ' }
+    phone_number         { '09012345678' }
+  end
+end
---
+++ spec/models/shopping_shipping_address_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe ShoppingShippingAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      sleep(1)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @shopping_shipping_address = FactoryBot.build(:shopping_shipping_address, item_id: item.id, user_id: item.user.id)
+    end
+
+    context '購入出来る場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@shopping_shipping_address).to be_valid
+      end
+      it 'building_nameは空でも保存できること' do
+        @shopping_shipping_address.building_name = ''
+        expect(@shopping_shipping_address).to be_valid
+      end
+    end
+
+    context '購入できない場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @shopping_shipping_address.postal_code = ''
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @shopping_shipping_address.postal_code = '1234567'
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
+      end
+      it 'area_idを選択していないと保存できないこと' do
+        @shopping_shipping_address.area_id = '1'
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'cityが空だと保存できないこと' do
+        @shopping_shipping_address.city = ''
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'house_numberが空だと保存できないこと' do
+        @shopping_shipping_address.house_number = ''
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("House number can't be blank")
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @shopping_shipping_address.phone_number = ''
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが9桁以下だと保存できないこと' do
+        @shopping_shipping_address.phone_number = '123456789'
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'userが紐付いていないと保存できないこと' do
+        @shopping_shipping_address.user_id = nil
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐付いていないと保存できないこと' do
+        @shopping_shipping_address.item_id = nil
+        @shopping_shipping_address.valid?
+        expect(@shopping_shipping_address.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
+end
