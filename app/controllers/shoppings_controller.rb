06a61aa5cb2db6c4b61404b835c830ed0a48c4a9
hanother3 <hanazawa.s58.1219@gmail.com>
2023-01-17 18:24:30 +0900

    決済機能のサーバーサイド実装

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -13,6 +13,7 @@ class ShoppingsController < ApplicationController
     @item = Item.find(params[:item_id])
     @shopping_shipping_address = ShoppingShippingAddress.new(shopping_params)
     if @shopping_shipping_address.valid?
+      pay_item
       @shopping_shipping_address.save
       redirect_to root_path
     else
@@ -22,8 +23,17 @@ class ShoppingsController < ApplicationController
   
   private
   
-    def shopping_params
-      params.require(:shopping_shipping_address).permit(:postal_code, :area_id, :city, :house_number, :phone_number, :building_name, :price).merge(item_id: @item.id, user_id: current_user.id)
-    end
+  def shopping_params
+    params.require(:shopping_shipping_address).permit(:postal_code, :area_id, :city, :house_number, :phone_number, :building_name, :price).merge(item_id: @item.id, user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_2818919976213aba7c0be3cf" 
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: shopping_params[:token],
+      currency: 'jpy'
+    )
+  end  
 
 end
